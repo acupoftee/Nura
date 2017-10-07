5bdb263391940191fe3021e91da65837e8487d1c
Bethy Diakabana <bethydiakabana@Bethys-MacBook-Pro.local>
2017-10-07 15:46:33 -0400

    Initial Commit

---
+++ Neural++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Neural++
+//
+//  Created by Bethy Diakabana on 10/7/17.
+//  Copyright © 2017 Bethy Diakabana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
