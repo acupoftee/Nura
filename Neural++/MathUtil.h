82cf91e51f90f8c272a508e7bc2537df76f48335
Bethy Diakabana <bethydiakabana@Bethys-MacBook-Pro.local>
2017-10-07 15:54:54 -0400

    initial commit

---
+++ Neural++/MathUtil.h
@@ -0,0 +1,18 @@
+//
+//  MathUtil.h
+//  Neural++
+//
+//  Created by Bethy Diakabana on 10/7/17.
+//  Copyright © 2017 Bethy Diakabana. All rights reserved.
+//
+
+#pragma once
+namespace neural {
+    class MathUtil {
+    public:
+        /**
+         * sigmoid function
+         */
+        static double sigmoid(double x);
+    }
+}
